56c459424a0385df887deea0cbebed93750a60e5
DYE <emil.dyekiss@gmail.com>
2014-05-10 20:10:03 +0200

    Removed original command file

--- Photons/photons.bat
+++
@@ -1,2 +0,0 @@
-java -jar photons.jar /home/emil/Képek/source/ /home/emil/Képek/imported/ jpg>/home/emil/Képek/imported/results.txt
-rem java -jar photons.jar /media/emil/ /home/emil/Képek/imported/ jpg
